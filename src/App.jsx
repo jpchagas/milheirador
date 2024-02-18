560548f76133711c7719b9203b0083940e8eabc7
João Pedro Chagas <jpchagas@gmail.com>
2024-02-18 16:26:47 -0300

    begin project

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [cards, setCards] = useState([
+    {
+      id: 1,
+      bandeira: "visa",
+      emissor: "bradesco", 
+      tipo: "infitine"
+  },
+  {
+      id:2,
+      bandeira: "american express",
+      emissor: "santander", 
+      tipo: "platinum"
+  }
+  ]);
+
+
+  return <div classNAme="app">
+      <h1> Lista de Cards</h1>
+      <div className="card-list">
+        {cards.map((card) => (
+          <div className="card">
+            <div className="content">
+              <p>{card.bandeira}</p>
+              <p className="category">{card.emissor}</p>
+              <p>{card.tipo}</p>
+            </div>
+            <div> Completar</div> 
+            <button>x</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  
+}
+
+export default App
